b3bd504b7f69cf210a35ba5a2a5733163b2598db
Induwara77 <induwaradilshan49@gmail.com>
2026-07-08 04:34:23 +0530

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 //middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://splitwise-clone-livid.vercel.app",
+    ],
     credentials: true,
   }),
 );
